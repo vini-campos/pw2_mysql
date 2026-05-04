e6da4c500e4db98af7ab02d5bc4911dd2064d981
vini-campos <viniciuscampos131209@gmail.com>
2026-05-04 10:55:12 -0300

    ajustes na conexão

--- Exemplos_aula/exemplo_mysqli/conexao.php
+++ Exemplos_aula/exemplo_mysqli/conexao.php
@@ -1,13 +1,24 @@
 ﻿<?php
 	mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 	$host = "localhost";
+	$banco = "banco";
 	$user = "root";
 	$pass = "";
-	$banco = "banco";
+	
+	try{
+		$pdo = new PDO("mysql:host={$host};dbname={$banco}", $user, $pass);
+
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
+	}catch(PDOException $e){
+		die("Erro na conexão" . $e->getMessage());
+	}
+
 
 	try {
 		$conexao = new mysqli($host, $user, $pass, $banco);
 		$conexao->set_charset("utf8");
+		//mysqli_set_charset($conexao, "utf8");
 	} catch (Exception $e) {
 		throw new Exception ("Problemas com a conexão do Banco de Dados : <br>" . $e->getMessage());
 	}
