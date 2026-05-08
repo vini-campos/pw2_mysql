13735f3b320fc6f574b35cbe465a918e05a51d0a
vini-campos <viniciuscampos131209@gmail.com>
2026-05-08 11:48:36 -0300

    adição de bootstrap no projeto

--- exemplo_mysqli/index.php
+++ exemplo_mysqli/index.php
@@ -4,11 +4,16 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" type="image/icon" href="img/icon.png">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<title>Exemplo PHP PW2</title>
 </head>
 
+<h3>Semana 01 - Exemplo 04 - Listagem Geral de Produtos - Imagem</h3>
+
 <body>
-	<h3>Semana 01 - Exemplo 04 - Listagem Geral de Produtos - Imagem</h3>
+	
+	<main class="container">
 		<?php
 		try {
 			include_once "conexao.php";
@@ -16,19 +21,19 @@
 			// ajustando a instrução select para ordenar por produto
 			$query = mysqli_query($conexao, "select * from tabelaimg order by produto");
 
-			if (!$query) {
-				die('Query Inválida: ' . @mysqli_error($conexao));
-			}
-
+			// if (!$query) {
+			// 	die('Query Inválida: ' . @mysqli_error($conexao));
+			// }
+		
 			echo "<table border='1px'>";// note que abri echo com aspas para executar
 			//comando html e os atributos das tags com apostrofe 
 			echo '<tr>
-					<th width="30px" align="center">Id</th>
-					<th width="100px">C&oacute;digo</th>
-					<th width="250px">Produto</th>
-					<th width="100px">Valor</th>
-					<th width="100px">Produto</th>
-				</tr>';
+				<th width="30px" align="center">Id</th>
+				<th width="100px">C&oacute;digo</th>
+				<th width="250px">Produto</th>
+				<th width="100px">Valor</th>
+				<th width="100px">Produto</th>
+			</tr>';
 
 			while ($dados = mysqli_fetch_array($query)) {
 				echo "<tr>";
@@ -37,17 +42,23 @@
 				echo "<td>" . $dados['produto'] . "</td>";
 				echo "<td align='right'> R$ " . $dados['valor'] . "</td>";
 				// buscando a na pasta imagem
-				echo "<td><img src='imagens/" . $dados['imagem'] . "'></td>";
+				echo "<td><img src='img/" . $dados['imagem'] . "'></td>";
 				echo "</tr>";
 			}
 			echo "</table>";
 
 			//mysqli_close($conexao);
 		} catch (Exception $e) {
-			echo "<h2> Aconteceu um erro: <br>" . $e->getMessage() . "";
+			echo "<div class=\"alert alert-danger\" role=\"alert\>\n
+				<h2> Aconteceu um erro: <br>\n
+				{$e->getMessage()}
+				</div>";
+			
 		}
 
 		?>
+	</main>
+	<script src="js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
